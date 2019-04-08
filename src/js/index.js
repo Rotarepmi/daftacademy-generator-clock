002bf085faa0318e968ea166fc1cf4ca4f7a7647
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-08 23:05:33 +0200

    Move Clock to separated module file

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,5 @@
+import Clock from './Clock';
 import '../css/style.css';
-
-class Clock {
-  constructor(currTime) {
-    this.sGen = this.generate(currTime.getSeconds());
-    this.mGen = this.generate(currTime.getMinutes());
-    this.hGen = this.generate(currTime.getHours() % 12, 11);
-
-    this.s = this.sGen.next().value;
-    this.m = this.mGen.next().value;
-    this.h = this.hGen.next().value;
-  }
-
-  runClock() {
-    if (this.m === 59 && this.s === 59) this.h = this.hGen.next().value;
-    if (this.s === 59) this.m = this.mGen.next().value;
-    this.s = this.sGen.next().value;
-  }
-
-  pad(num) {
-    return num.toString().padStart(2, '0');  // padStart - the ES2017 function which fulfills strings
-  }
-
-  *generate(start = 0, threshold = 59) {
-    while (true) {
-      const curr = start++;
-      yield curr;
-      if (curr === threshold) start = 0;
-    }
-  }
-}
 
 (function run() {
   const hCont = document.querySelector('#h');
